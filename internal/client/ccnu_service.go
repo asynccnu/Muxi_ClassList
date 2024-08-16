619e5d4f9636dab18c55fc804c7e3b39a6cf1df3
chencheng8888 <2963801553@qq.com>
2024-08-16 20:44:56 +0800

    服务发现

---
+++ internal/client/ccnu_service.go
@@ -0,0 +1,32 @@
+package client
+
+import (
+	v1 "class/api/classer/v1"
+	"context"
+	"github.com/go-kratos/kratos/contrib/registry/etcd/v2"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/middleware/tracing"
+	"github.com/go-kratos/kratos/v2/transport/grpc"
+)
+
+const (
+	CCNU_SERVICE = "discovery:///ccnu_service"
+)
+
+func NewClient(r *etcd.Registry, logger log.Logger) (v1.ClasserClient, error) {
+	conn, err := grpc.DialInsecure(
+		context.Background(),
+		grpc.WithEndpoint(CCNU_SERVICE), // 需要发现的服务，如果是k8s部署可以直接用服务器本地地址:9001，9001端口是需要调用的服务的端口
+		grpc.WithDiscovery(r),
+		grpc.WithMiddleware(
+			tracing.Client(),
+			recovery.Recovery(),
+		),
+	)
+	if err != nil {
+		log.NewHelper(logger).WithContext(context.Background()).Errorw("kind", "grpc-client", "reason", "GRPC_CLIENT_INIT_ERROR", "err", err)
+		return nil, err
+	}
+	return v1.NewClasserClient(conn), nil
+}
