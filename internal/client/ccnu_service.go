5f913d337fc6e51e15dc957e93c0a7b5c5b4dc84
chencheng8888 <2963801553@qq.com>
2024-08-16 20:51:32 +0800

    服务发现(小修改)

--- internal/client/ccnu_service.go
+++ internal/client/ccnu_service.go
@@ -1,8 +1,8 @@
 package client
 
 import (
-	v1 "class/api/classer/v1"
 	"context"
+	v1 "github.com/asynccnu/ccnu-service/api/ccnu_service/v1"
 	"github.com/go-kratos/kratos/contrib/registry/etcd/v2"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
@@ -14,7 +14,7 @@ const (
 	CCNU_SERVICE = "discovery:///ccnu_service"
 )
 
-func NewClient(r *etcd.Registry, logger log.Logger) (v1.ClasserClient, error) {
+func NewClient(r *etcd.Registry, logger log.Logger) (v1.CCNUServiceClient, error) {
 	conn, err := grpc.DialInsecure(
 		context.Background(),
 		grpc.WithEndpoint(CCNU_SERVICE), // 需要发现的服务，如果是k8s部署可以直接用服务器本地地址:9001，9001端口是需要调用的服务的端口
@@ -28,5 +28,5 @@ func NewClient(r *etcd.Registry, logger log.Logger) (v1.ClasserClient, error) {
 		log.NewHelper(logger).WithContext(context.Background()).Errorw("kind", "grpc-client", "reason", "GRPC_CLIENT_INIT_ERROR", "err", err)
 		return nil, err
 	}
-	return v1.NewClasserClient(conn), nil
+	return v1.NewCCNUServiceClient(conn), nil
 }
